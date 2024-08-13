acdb2ab776d36302f0f08c49fa0d9be89347e298
yeshu10 <yeshvigupta10@gmail.com>
2024-08-13 11:34:35 +0530

    logo added

--- Randomusers/src/components/RandomUserProfile.jsx
+++ Randomusers/src/components/RandomUserProfile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import cup from '../assets/cup-logo.svg';
 import { FaArrowLeft, FaRedo, FaMapMarkerAlt, FaPhoneAlt } from 'react-icons/fa'; // Added icons
 
 const RandomUserProfile = () => {
@@ -43,46 +44,54 @@ const RandomUserProfile = () => {
           <div className='text-center'>
             <img src={user.picture.large || 'https://via.placeholder.com/150'} alt="User" className='rounded-full w-32 h-32 mb-4 mx-auto' />
             <p className='text-lg font-semibold'>{user.name.first} {user.name.last}</p>
-            <p className='text-lg mb-4'>
-              <span className='font-semibold'>Location:</span> 
-              <FaMapMarkerAlt className='inline text-yellow-500 ml-2' /> 
-              <a href={`https://www.google.com/maps?q=${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state}, ${user.location.country}`} target="_blank" rel="noopener noreferrer" className='text-blue-600 hover:underline'>
-                {user.location.street.number} {user.location.street.name}, {user.location.city}, {user.location.state}, {user.location.country}
+            <div className='flex items-center justify-center mb-4 space-x-4'>
+              <a href={`https://www.google.com/maps?q=${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state}, ${user.location.country}`} target="_blank" rel="noopener noreferrer" className='flex items-center space-x-2'>
+                <div className='inline-flex items-center justify-center w-8 h-8 bg-black text-white rounded-full'>
+                  <FaMapMarkerAlt size={16} />
+                </div>
+                <span className='font-semibold'>Location</span>
               </a>
-            </p>
-            <p className='text-lg mb-4'>
-              <FaPhoneAlt className='inline text-yellow-500 mr-2' /> 
-              <a href={`tel:${user.phone}`} className='text-blue-600 hover:underline'>Call me</a>
-            </p>
+              <a href={`tel:${user.phone}`} className='flex items-center space-x-2'>
+                <div className='inline-flex items-center justify-center w-8 h-8 bg-black text-white rounded-full'>
+                  <FaPhoneAlt size={16} />
+                </div>
+                <span>Call me</span>
+              </a>
+            </div>
             <div className='grid grid-cols-2 gap-4 text-left'>
               <div>
-                <p className='text-lg'>
+                <p className='text-xs mb-2'>
                   <span className='font-semibold'>City:</span><br />
                   {user.location.city}
                 </p>
-                <p className='text-lg'>
+                <p className='text-xs mb-2'>
                   <span className='font-semibold'>Date of Birth:</span><br />
                   {user.dob.date}
                 </p>
-                <p className='text-lg'>
+                <p className='text-xs mb-2'>
                   <span className='font-semibold'>Time Zone:</span><br />
                   {user.location.timezone.offset}
                 </p>
               </div>
               <div>
-                <p className='text-lg'>
+                <p className='text-xs mb-2'>
                   <span className='font-semibold'>Nationality:</span><br />
                   {user.nat}
                 </p>
-                <p className='text-lg'>
+                <p className='text-xs mb-2'>
                   <span className='font-semibold'>Phone:</span><br />
                   {user.phone}
                 </p>
-                <p className='text-lg'>
+                <p className='text-xs mb-2'>
                   <span className='font-semibold'>Registered Since:</span><br />
                   {user.registered.date}
                 </p>
               </div>
+            </div>
+            <div className='mt-6 flex justify-end'>
+              <a href='https://chaicode.com' target="_blank" rel="noopener noreferrer" className='flex items-center justify-center bg-black p-2 rounded-lg'>
+                <img src={cup} alt='Chaicode Logo' className='w-12 h-12 rounded-lg' />
+              </a>
             </div>
           </div>
         )}
